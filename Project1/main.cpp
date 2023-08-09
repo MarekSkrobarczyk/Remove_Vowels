f9d54d22d4e09920ebea624ca8815a7ad74040d7
Marek Skrobarczyk <marekskrobarczyk@gmail.com>
2023-08-09 10:37:10 +0200

    Declaration of libraries and functions

---
+++ Project1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+bool isVowel(char toCheck); // Check which string elements are vowels
+std::string removeVowel(std::string); //Removing vowels from a string
+
+int main()
+{
+
+	return 0;
+}
